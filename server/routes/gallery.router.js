e5c8c32b66b01e6fd69e2a44cb2a46a5b4a98de1
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-02 17:52:46 -0500

    updated the put request to work with the database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -7,18 +7,21 @@ const pool = require('../modules/gallery.data');
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const galleryId = req.params.id/1;
+    const queryText = `UPDATE pictures SET likes = likes+1 WHERE "id"=$1;`
+
+    pool.query(queryText, [galleryId])
+    .then(result => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.log(`Error making query ${queryText}`, err);
+        res.sendStatus(500);
+    });
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    let queryText ="SELECT * FROM pictures;"
+    let queryText ="SELECT * FROM pictures ORDER BY id ASC;"
      pool.query(queryText)
     .then((response) => {
         res.send(response.rows);
