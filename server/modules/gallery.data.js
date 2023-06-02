d570bcddbdd01a6366a2719a74a62e235bfa8e45
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-02 17:03:56 -0500

    udated the description of goat 3

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,7 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small_1.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 1 },
     { id: 2, path: 'images/goat_small_2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small_3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/goat_small_3.jpg', description: '.ʞɹɐԀ ʅɐuoᴉʇɐN ɹǝᴉɔɐʅ⅁ ʇɐ uǝʞɐʇ ʇɐoƃ ɐ ⅎo oʇoɥԀ', likes: 0 },
     { id: 4, path: 'images/goat_small_4.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
 ];
 
