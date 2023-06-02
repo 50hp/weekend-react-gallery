e646a19c435d78075f1ecd2b258ae2e83ca65290
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-02 16:58:22 -0500

    created a function and terinary to show description of immage when a picture is clicked.

--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -1,14 +1,38 @@
-
+import {useState} from 'react';
 
 function GalleryItem(props) {
-const putLikes = props.putLikes;
-const item = props.item;
+    const putLikes = props.putLikes;
+    const item = props.item;
 
+    const [toggle, setToggle] = useState(0);
+    const swappToggle = () => {
+            if (toggle === 0){
+                setToggle(1);    
+            }else {
+                setToggle(0);
+            }
+
+
+    }
     return (
 
         <div className="Box">
+            <div className="pic" onClick={()=> swappToggle()}>
+            {(toggle===0) ? (
 
-            <img src={item.path}/>
+                    <img src={item.path}/>
+
+            ):(
+
+                    <p>{item.description}</p>
+
+            )}
+
+
+
+
+            </div>
+
             <button onClick={()=> putLikes(item.id)}>I Like This</button>
             <p>{item.likes} people like this!</p>
 
