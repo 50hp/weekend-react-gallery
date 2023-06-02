60d3534e626d1bc4b97ce47c9dae1998582bfdb5
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-02 16:47:06 -0500

    updated pool

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 1 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/goat_small_1.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 1 },
+    { id: 2, path: 'images/goat_small_2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/goat_small_3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/goat_small_4.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
 ];
 
 module.exports = galleryItems;
